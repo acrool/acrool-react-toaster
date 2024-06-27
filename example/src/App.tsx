394d24e30a91e2efa027d6e2eda0bcbc809dbe0f
imagine10255 <imagine10255@gmail.com>
2024-06-27 14:46:20 +0800

    modify reademe

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,37 +1,15 @@
-import {EStatus, toast, ToasterPortal} from '@acrool/react-toaster';
-import Github from './assets/github.svg?react';
+import {ToasterPortal} from '@acrool/react-toaster';
 
 import './App.css';
 import Example from './views/Example';
+import Banner from './components/Banner';
 
 
 
 function App() {
-
-
-    const renderHeader = () => {
-
-        const repositoryUrl = 'https://github.com/acrool/acrool-react-toaster';
-        const name = 'Acrool React Toaster';
-
-        return <>
-            <a href={repositoryUrl} target="_blank" rel="noopener noreferrer">
-                <Github width={40} height={40}/>
-            </a>
-
-            <div className="banner-wrapper">
-                <img src="/logo.svg" alt={name}/>
-                <h1>{name}</h1>
-            </div>
-        </>;
-    };
-
-
     return (
         <div className="App">
-            {renderHeader()}
-
-
+            <Banner/>
             <Example/>
 
             <ToasterPortal defaultTimeout={5000}/>
