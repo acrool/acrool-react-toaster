2fc130272f894852abe0113c2f4fd99e7bb0301c
imagine10255 <imagine10255@gmail.com>
2024-11-27 15:42:20 +0800

    export IToasterWrapperProps type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export {default as ToasterPortal, toast} from './Toaster';
 export {default as ToasterWrapper} from './ToasterWrapper';
+export * from './ToasterWrapper';
 export * from './Toaster';
 export * from './types';
 
